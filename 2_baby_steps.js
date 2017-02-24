66c3a747c15bc275addaff72a34e6f371e89d693
lidia-dx <lidia.khmylko@gmail.com>
2017-02-24 12:55:13 -0800

    Extended the comment

--- 2_baby_steps.js
+++ 2_baby_steps.js
@@ -7,6 +7,10 @@
  * arguments and prints the sum of those numbers to the console (stdout).
  */
 
+
+// process.argv is an array of program parameters of the form:
+// [ 'path/to/node', 'path/to/your/program', 'arg1', 'arg2']
+
 var sum = 0;
 
 if (process.argv.length > 2) {
