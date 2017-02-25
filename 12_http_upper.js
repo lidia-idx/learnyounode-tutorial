c4b89d289252a0a9c0b9617743880af42fc90fe9
lidia-dx <lidia.khmylko@gmail.com>
2017-02-24 18:06:21 -0800

    task 12 done

---
+++ 12_http_upper.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Lidia Khmylko on 2/21/15.
+ * Licensed under MIT License.
+ *
+ * TASK 12. HTTP UPPERCASERER
+ *
+ * Write an HTTP server that receives only POST requests and converts
+ * incoming POST body characters to upper-case and returns it to the client.
+ *
+ * Your server should listen on the port provided by the first argument to
+ * your program.
+ */
+
+var http = require('http');
+var map = require('through2-map');
+
+var port = process.argv[2];
+
+var server = http.createServer(function(request, response){
+
+    request.pipe(map(function(chunk){
+        return chunk.toString().toUpperCase();
+    })).pipe(response);
+});
+
+
+server.listen(port);
