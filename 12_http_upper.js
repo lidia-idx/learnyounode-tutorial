e5601d7933fbbdbfac066b5e99361f0f286955e0
lidia-dx <lidia.khmylko@gmail.com>
2017-02-24 18:07:09 -0800

    port should be a number

--- 12_http_upper.js
+++ 12_http_upper.js
@@ -14,7 +14,7 @@
 var http = require('http');
 var map = require('through2-map');
 
-var port = process.argv[2];
+var port = +process.argv[2];
 
 var server = http.createServer(function(request, response){
 
